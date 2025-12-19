1f365c5502832ed89a74c6a0c2242e1d58fe958b
amninder-narota <163357823+amninder-narota@users.noreply.github.com>
2025-12-19 17:06:44 -0500

    fix: use nerd font f056 for deleted files in nvim-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -59,7 +59,7 @@ return {
               unmerged = '', -- Merge conflict
               renamed = '➜', -- Renamed
               untracked = '󰃅', -- New/added file (nerd font f00c5)
-              deleted = '', -- Deleted (nerd font f111)
+              deleted = '', -- Deleted (nerd font f056)
               ignored = '◌', -- Git ignored
             },
           },
